94b2719f7b99e8fcf2b5587fbb4b8e8c9ecc714b
quandangiu <quannd.22itb@vku.udn.vn>
2025-04-24 17:09:49 +0700

    Update lab7 code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lab_flutter/view/StoryScreen.dart';
 import 'package:lab_flutter/view/quiScreen.dart';
 
 void main() {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: QuizPage(),
+      home: Destini(),
     );
   }
 }
